d804bebeefa1eae71120de0f6a2a46a58edaf9cd
ausfelt <121190890+ausfelt@users.noreply.github.com>
2022-12-26 19:39:12 +0100

    Create task3.py

---
+++ homework/lesson2/task3.py
@@ -0,0 +1,25 @@
+# This is a "calculator" to show how simple maths work in python.
+
+a = 6
+b = 26
+
+# addition
+print(a + b)
+
+# subtraction
+print(a - b)
+
+# division
+print(a / b)
+
+# multiplication
+print(a * b)
+
+# exponent
+print(a ** b)
+
+# modulus
+print(a % b)
+
+# floor division
+print(a // b)
