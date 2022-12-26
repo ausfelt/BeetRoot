c91a80480feb725a2cd75c9827e1b9c323ed8638
ausfelt <121190890+ausfelt@users.noreply.github.com>
2022-12-26 19:35:19 +0100

    Create task2.py

---
+++ homework/lesson2/task2.py
@@ -0,0 +1,9 @@
+# Manipulate strings.
+
+# Save your first and last name as separate variables, then use string concatenation
+# to add them together with a white space in between and print a greeting.
+
+first_name = "Linus"
+last_name = "Ausfelt"
+
+print("My name is " + first_name + " and the name my father gave me is " + last_name)
