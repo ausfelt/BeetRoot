0e52e9af36cf23b1a9b5e140d214a404912c7554
ausfelt <121190890+ausfelt@users.noreply.github.com>
2022-12-26 18:50:38 +0100

    Create task2.py

---
+++ homework/lesson1/task2.py
@@ -0,0 +1,32 @@
+# Create a python program named “task2”, and use the built-in function `print` in it several times.
+# to pass “sep”, “end” params and pass several parameters separated by commas.
+# Also, provide a comment text above each print statement,
+# mentioned above, with the expected output after execution of the particular print statement.
+
+
+a = "Hello"
+b = "there"
+# Hello there
+
+print(a,b)
+
+c= ", I"
+d= "love"
+e = "big"
+f = "cakes"
+
+
+g = "and my passion is"
+r= "horses"
+
+
+# Hello there , I love big 
+
+# cakes and my passion is 
+
+
+# horses
+
+print(a,b,c,d,e,f, sep= " ", end = '\n\n')
+print(g, end= '\n\n\n')
+print (r, end ='\n')
