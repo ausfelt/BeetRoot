c224e3590b89064f1818d9fa5335b27c405290d6
ausfelt <121190890+ausfelt@users.noreply.github.com>
2022-12-26 18:54:06 +0100

    Create task3.py

---
+++ homework/lesson1/task3.py
@@ -0,0 +1,15 @@
+# This will make it say O H in ASCII and it will be printed exactly as written except for the ''' and print part.
+
+
+print('''#########
+#       #
+#       #
+#       #
+#########
+
+
+#       #
+#       #
+######### 
+#       # 
+#       #''')
