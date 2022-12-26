0c02939d286010b40b9e7cdd7092425bb1eb48e7
ausfelt <121190890+ausfelt@users.noreply.github.com>
2022-12-26 19:30:05 +0100

    Create task1.py

---
+++ homework/lesson2/task1.py
@@ -0,0 +1,11 @@
+# Task 1
+
+# "Good day <name>! <day> is a perfect day to learn some python."
+
+# Make a greeting program!
+
+name = "Linus"
+current_day = "Monday"
+
+
+print("Hello", name,"!", current_day, "is a perfect day to learn some python.")
