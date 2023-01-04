b40bc82ebaba7fff09d9f185143f60c11a763686
ausfelt <121190890+ausfelt@users.noreply.github.com>
2023-01-04 20:21:02 +0100

    Create 040123.py

---
+++ tests/040123.py
@@ -0,0 +1,21 @@
+#increment by 1
+incrementing_number = 0
+
+while(incrementing_number <= 10):
+    print(incrementing_number)
+    incrementing_number += 1
+
+#increment by 2
+incrementing_number = 0
+
+while(incrementing_number <= 10):
+    print(incrementing_number)
+    incrementing_number += 2
+
+#increment by 1
+for incrementing_number in range(0, 11):
+    print(incrementing_number)
+
+#increment by 2
+for incrementing_number in range(0, 6):
+    print(2 * incrementing_number)
