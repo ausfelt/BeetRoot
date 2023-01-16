4d36d30d4101fd8ffc00ba2537470a862949865f
ausfelt <121190890+ausfelt@users.noreply.github.com>
2023-01-16 14:50:38 +0100

    Create task1.py

---
+++ homework/functions/task1.py
@@ -0,0 +1,12 @@
+'''A simple function.
+
+Create a simple function called favorite_movie, which takes a string
+containing the name of your favorite movie. The function should then
+print "My favorite movie is named {name}".'''
+
+def favorite_movie(movie):
+    print(f"My favorite movie is also {movie}")
+
+
+movie = input("Which is your favorite movie? \n")
+favorite_movie(movie)
