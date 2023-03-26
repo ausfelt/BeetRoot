b0786c4341f25322d0f4c50f92fd8fa49569eb86
ringstorp <ausligger@gmail.com>
2023-03-26 12:53:37 +0200

    test changes

--- homework/Exceptions/task1.py
+++ homework/Exceptions/task1.py
@@ -1,1 +1,22 @@
-print("hejhej")
+class Klass:
+    some_attr = "Klass"
+    # Klass has its namespace. It's stored in Klass.__dict__.
+    # And after we said some_attr = "Klass" Klass.__dict__ got a record {"some_attr": "Klass"}
+
+    def __init__(self):
+        self.some_attr = "not Klass"
+        # we create an instance, and its __dict__ is going to receive {"some_attr": "not Klass"}
+
+    def print_some_attr(self):
+        print(self.some_attr)
+
+    @classmethod
+    def print_some_class_attr(cls):
+        print(cls.some_attr)
+
+
+klass_instance = Klass()
+klass_instance.print_some_attr()
+klass_instance.print_some_class_attr()
+klass_instance.
+
---
+++ homework/http/http.py
@@ -0,0 +1,4 @@
+from requests_html import HTMLSession
+session = HTMLSession()
+
+r = session.get("https://wikipedia.org")
