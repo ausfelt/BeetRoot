e3366950c28b7243bc14f5d005f0134357228097
ringstorp <ausligger@gmail.com>
2023-03-28 20:27:29 +0200

    uploaded HW

--- homework/dictionaries_for_loops_comprehensions/task1.py
+++ homework/dictionaries_for_loops_comprehensions/task1.py
@@ -1,17 +1,14 @@
-"""
-Make a program that has some sentence (a string) on input and returns a dict containing all unique words as keys
-and the number of occurrences as values.
-"""
+
 
 sentence = input("Type a sentence without punctuation here, please: ")
 keys = sentence.lower().split()
 
 our_dictionary = {}
 
-for element in keys:
-    if element in our_dictionary:
-        our_dictionary[element] += 1
+for words in keys:
+    if words in our_dictionary:
+        our_dictionary[words] += 1
     else:
-        our_dictionary[element] = 1
+        our_dictionary[words] = 1
 
 print(our_dictionary)
--- homework/dictionaries_for_loops_comprehensions/task2.py
+++ homework/dictionaries_for_loops_comprehensions/task2.py
@@ -1,23 +1,9 @@
-'''Task 2
-Input data:
-stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15
-}
-prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
-}
-Create a function which takes as input two dicts with structure mentioned above, then computes and returns the total price of stock.'''
 
-def total_price(dict1, dict2):
+
+def total_price(dictionary_1, dictionary_2):
     total_price = 0
-    for key in dict1:
-        total_price += dict1[key]*dict2[key]
+    for key in dictionary_1:
+        total_price += dictionary_1[key] * dictionary_2[key]
     return total_price
 
 stock = {
